9173ba54bf4b153afd85a25a0b24c3709ffc8fe6
Santeri Heiskanen <heiskanens98@gmail.com>
2025-09-18 15:52:55 +0300

    Remove deadcode

--- train.py
+++ train.py
@@ -289,11 +289,6 @@ def sampling(
             If num_pareto_solutions is not divisible by conditioning points.
     """
     # assert config.sampling_method in ("uniform-ideal", "uniform-direction", )
-
-    print(
-        f"Sampling for {cond_points_tensor.shape[0]} solutions! "
-        f"with {cond_points_tensor.shape=} and scale {guidance_scale:.2f}"
-    )
     res_x = diffusion.sample(
         batch_size=cond_points.shape[0],
         cond=cond_points,
